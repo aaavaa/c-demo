52492cd858435daa51f3df1e989bac1995df5b99
aava <aavayonzan@gmail.com>
2022-02-06 00:04:52 +0545

    implemented taking input from user for subtraction.

---
+++ subtraction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int first_number, second_number;
+
+    printf("\nEnter two numbers: ");
+    scanf("%d %d", &first_number, &second_number);
+
+    return 0;
+}

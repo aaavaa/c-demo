9984f3cf238fdaf45609875a174d478b166bb29f
aava <aavayonzan@gmail.com>
2022-02-06 00:09:40 +0545

    refactor: remove unnecessary whitespaces.

--- subtraction.c
+++ subtraction.c
@@ -3,12 +3,9 @@
 int main(void)
 {
     int first_number, second_number;
-
     printf("\nEnter two numbers: ");
     scanf("%d %d", &first_number, &second_number);
-
     int difference = first_number - second_number;
-
     printf("The difference is %d", difference);
     return 0;
 }

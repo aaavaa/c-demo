6b9773411f79a90728ceb872c93aa857a38faaef
aava <aavayonzan@gmail.com>
2022-02-06 00:07:24 +0545

    implemented logic for subtraction.

--- subtraction.c
+++ subtraction.c
@@ -7,5 +7,8 @@ int main(void)
     printf("\nEnter two numbers: ");
     scanf("%d %d", &first_number, &second_number);
 
+    int difference = first_number - second_number;
+
+    printf("The difference is %d", difference);
     return 0;
 }
